b3ca8a20143089714d8a0620d4c9c41c2269bc43
frederic jeanne <fredericjeanne@gmail.com>
2023-04-28 16:29:35 +0200

    seed modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Spree::Core::Engine.load_seed
 Spree::Auth::Engine.load_seed
+
+# frozen_string_literal: true
+
+require 'thor'
+shell = Thor::Base.shell.new
+
+%w(
+  stores
+  store_credit
+  countries
+  return_reasons
+  states
+  stock_locations
+  zones
+  refund_reasons
+  roles
+  shipping_categories
+).each do |seed|
+  shell.say_status :seed, seed
+  require_relative "default/spree/#{seed}"
+end
